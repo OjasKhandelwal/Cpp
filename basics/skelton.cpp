73b1f652a8867d7f7ad42a7ae38159e374d29169
Ojas Khandelwal <ojascancode@gmail.com>
2025-08-21 09:55:22 +0530

    Create skelton.cpp

---
+++ basics/skelton.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <bits/stdc++.h>  //includes all the libraries
+using namespace std;  //this uses std in every command by default
+int main() {
+    
+    cout<< "hello world" << "\n";
+    cout<< "what??????"<< endl; //endl is alt for \n only 
+    
+    cout<< "learning" ;  
+    cout<< " for the 1st time \n";
+    
+    int x,y,z;
+    cout<< "enter a number:";
+    cin >> x;
+    cout<< "value of x:" << x;
+    
+    cout<< "\nenter 2 numbers:";
+    cin >> y >>z;
+    cout<< "value of y:" << y << " & z:"<<z;
+
+    return 0;
+}
