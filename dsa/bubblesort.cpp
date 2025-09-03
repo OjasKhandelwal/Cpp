025cd9d33227dea824f3cb9261ea5504633e3bfa
Ojas Khandelwal <ojascancode@gmail.com>
2025-09-03 13:50:46 +0530

    Fix bubble sort loop structure in bubblesort.cpp

--- dsa/bubblesort.cpp
+++ dsa/bubblesort.cpp
@@ -5,8 +5,8 @@ int main(){
     //bubble sort
     int arr[8] = {54,5,43,25,56,64,7,68};
     int n = 8;
-    for(int i = 0; i<n-1;i++){
-        for(int j = 0; j < n-1-i; j++){
+    for(int i = 0; i<n-1;i++){   //passing on elements
+        for(int j = 0; j < n-1-i; j++){  //comparing 
             if(arr[j]>arr[j+1]){
                 swap(arr[j],arr[j+1]);
                 
@@ -14,6 +14,7 @@ int main(){
             
         }
     }
+    cout<<"sorted array : ";
     for(int i = 0; i<n  ; i++){
         cout<<arr[i]<<" ";
         
